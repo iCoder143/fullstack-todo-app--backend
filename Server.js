991ec2fd635c16fc420f793cc360147b60f0d253
iCoder143 <92156328+iCoder143@users.noreply.github.com>
2023-05-17 15:05:21 +0500

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes =require("./Routes/ToDoRoutes")
+const cors =require('cors')
+
+
+require('dotenv').config()
+const app = express()
+const PORT = process.env.port || 5000
+
+app.use(express.json())
+app.use(cors())
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log(`Connected to MongoDB...`))
+    .catch((err) => console.log(err))
+
+    app.use(routes)
+
+app.listen(PORT, () => console.log(`Listning to PORT: ${PORT}...`))
